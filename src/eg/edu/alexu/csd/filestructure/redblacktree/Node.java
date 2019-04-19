3d6b3aff7d1802e6169993469e3368ea9786210e
muhammad salah <msalah.29.10@gmail.com>
2019-04-19 19:45:25 +0200

    initial commit

---
+++ src/eg/edu/alexu/csd/filestructure/redblacktree/Node.java
@@ -0,0 +1,83 @@
+package eg.edu.alexu.csd.filestructure.redblacktree;
+
+public class Node implements INode{
+
+	@Override
+	public void setParent(INode parent) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public INode getParent() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setLeftChild(INode leftChild) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public INode getLeftChild() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setRightChild(INode rightChild) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public INode getRightChild() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Comparable getKey() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setKey(Comparable key) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Object getValue() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setValue(Object value) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean getColor() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void setColor(boolean color) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isNull() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
